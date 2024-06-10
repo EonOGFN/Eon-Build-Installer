adcb944452fa3c3397657908a089d03e0d6a9973
Volcano-MP <149071205+Project-Z1K@users.noreply.github.com>
2024-06-10 13:45:59 +0000

    downloading fix v2

--- Installer/Installer/Installer.cs
+++ Installer/Installer/Installer.cs
@@ -12,7 +12,10 @@ namespace Eon_Installer.Installer
 {
     internal class Installer
     {
-        private static readonly HttpClient httpClient = new HttpClient();
+        private static readonly HttpClient httpClient = new HttpClient
+        {
+            Timeout = TimeSpan.FromMinutes(10) // Adjust timeout as necessary
+        };
 
         public static async Task Download(ManifestFile manifest, string version, string resultPath)
         {
@@ -24,73 +27,78 @@ namespace Eon_Installer.Installer
                 Directory.CreateDirectory(resultPath);
 
             using SemaphoreSlim semaphore = new SemaphoreSlim(12);
+            List<Task> downloadTasks = new List<Task>();
 
-            var downloadTasks = manifest.Chunks.Select(async chunkedFile =>
+            foreach (var chunkedFile in manifest.Chunks)
             {
-                await semaphore.WaitAsync();
-
-                try
+                downloadTasks.Add(Task.Run(async () =>
                 {
-                    string outputFilePath = Path.Combine(resultPath, chunkedFile.File);
-                    var fileInfo = new FileInfo(outputFilePath);
-
-                    if (File.Exists(outputFilePath) && fileInfo.Length == chunkedFile.FileSize)
+                    await semaphore.WaitAsync();
+                    try
                     {
-                        Interlocked.Add(ref completedBytes, chunkedFile.FileSize);
-                        return;
-                    }
+                        string outputFilePath = Path.Combine(resultPath, chunkedFile.File);
+                        var fileInfo = new FileInfo(outputFilePath);
 
-                    Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath));
-
-                    using FileStream outputStream = File.Open(outputFilePath, FileMode.Create, FileAccess.Write, FileShare.None);
-
-                    foreach (int chunkId in chunkedFile.ChunksIds)
-                    {
-                        bool success = false;
-                        while (!success)
+                        if (File.Exists(outputFilePath) && fileInfo.Length == chunkedFile.FileSize)
                         {
-                            try
-                            {
-                                string chunkUrl = $"{Globals.SeasonBuildVersion}/{version}/{chunkId}.chunk";
-                                var chunkData = await httpClient.GetByteArrayAsync(chunkUrl);
-
-                                using MemoryStream memoryStream = new MemoryStream(chunkData);
-                                using GZipStream decompressionStream = new GZipStream(memoryStream, CompressionMode.Decompress);
-
-                                byte[] buffer = new byte[Globals.CHUNK_SIZE];
-                                int bytesRead;
-                                while ((bytesRead = await decompressionStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
-                                {
-                                    await outputStream.WriteAsync(buffer, 0, bytesRead);
-                                    Interlocked.Add(ref completedBytes, bytesRead);
-
-                                    double progress = (double)completedBytes / totalBytes * 100;
-                                    string progressMessage = $"\rDownloaded: {ConvertStorageSize.FormatBytesWithSuffix(completedBytes)} / {ConvertStorageSize.FormatBytesWithSuffix(totalBytes)} ({progress:F2}%)";
-                                    Console.Write(progressMessage.PadRight(progressLength));
-                                    progressLength = progressMessage.Length;
-                                }
-
-                                success = true;
-                            }
-                            catch (Exception)
-                            {
-                                // Log exception or implement retry policy
-                                // Wait a bit before retrying to avoid spamming the server
-                                await Task.Delay(500);
-                            }
+                            Interlocked.Add(ref completedBytes, chunkedFile.FileSize);
+                            return;
                         }
+
+                        Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath));
+
+                        using FileStream outputStream = new FileStream(outputFilePath, FileMode.Create, FileAccess.Write, FileShare.None);
+
+                        List<Task> chunkDownloadTasks = chunkedFile.ChunksIds.Select(chunkId => DownloadChunk(version, chunkId, outputStream, ref completedBytes, totalBytes)).ToList();
+                        await Task.WhenAll(chunkDownloadTasks);
                     }
-                }
-                finally
-                {
-                    semaphore.Release();
-                }
-            });
+                    finally
+                    {
+                        semaphore.Release();
+                    }
+                }));
+            }
 
             await Task.WhenAll(downloadTasks);
 
             Console.WriteLine("\n\nFinished Downloading.\nPress any key to exit!");
             Console.ReadKey();
+        }
+
+        private static async Task DownloadChunk(string version, int chunkId, FileStream outputStream, ref long completedBytes, long totalBytes)
+        {
+            bool success = false;
+            while (!success)
+            {
+                try
+                {
+                    string chunkUrl = $"{Globals.SeasonBuildVersion}/{version}/{chunkId}.chunk";
+                    var chunkData = await httpClient.GetByteArrayAsync(chunkUrl);
+
+                    using MemoryStream memoryStream = new MemoryStream(chunkData);
+                    using GZipStream decompressionStream = new GZipStream(memoryStream, CompressionMode.Decompress);
+
+                    byte[] buffer = new byte[Globals.CHUNK_SIZE];
+                    int bytesRead;
+                    while ((bytesRead = await decompressionStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                    {
+                        await outputStream.WriteAsync(buffer, 0, bytesRead);
+                        Interlocked.Add(ref completedBytes, bytesRead);
+
+                        double progress = (double)completedBytes / totalBytes * 100;
+                        string progressMessage = $"\rDownloaded: {ConvertStorageSize.FormatBytesWithSuffix(completedBytes)} / {ConvertStorageSize.FormatBytesWithSuffix(totalBytes)} ({progress:F2}%)";
+                        Console.Write(progressMessage.PadRight(50));
+                    }
+
+                    success = true;
+                }
+                catch (Exception)
+                {
+                    // Log exception or implement retry policy
+                    // Wait a bit before retrying to avoid spamming the server
+                    await Task.Delay(500);
+                }
+            }
         }
     }
 }
